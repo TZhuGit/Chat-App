1dc1a115a18b33abdfddaf53924a4d18d45b33c6
Zhexi (Tony) Zhu <xzhu228@uwo.ca>
2019-05-20 19:41:20 -0700

    Update to latest express/socket io version

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+server.listen(port);
